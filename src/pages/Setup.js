a1605aec76ec7ddc2799acc83a64e96bab0ba668
leeseonj <leeseonj1214@gmail.com>
2025-08-16 01:08:21 +0900

    Complete_extremebasic_skel

--- src/pages/Setup.js
+++ src/pages/Setup.js
@@ -4,6 +4,7 @@ import SetupCheckAsset from './Setup_checkAsset';
 import Setupsaving from './Setup_savings'; 
 import Setupinvestment from './Setup_investment';
 import SetupMyAsset from './Setup_MyAsset';
+import SetupMyLoan from './Setup_MyLoan';
 import {useState} from 'react';
 
 
@@ -17,8 +18,8 @@ export default function Setup({onNext, onPrev}) {
       {state === 1 && <SetupCheckAsset onNext = {() => setState(2)} onPrev = {() => setState(0)} />}
       {state === 2 && <Setupsaving onNext = {() => setState(3)} onPrev = {() => setState(1)} /> }
       {state === 3 && <Setupinvestment onNext = {() => setState(4)} onPrev = {() => setState(2)} /> }
-      {state === 4 && <SetupMyAsset onNext = {onNext} onPrev = {() => setState(3)} /> }
-      {/* {state === 5 && <SetupAssetsChangeRate onNext = {() => setState(6)} onPrev = {() => setState(4)} /> } */}
+      {state === 4 && <SetupMyAsset onNext = {() => setState(5)} onPrev = {() => setState(3)} /> }
+      {state === 5 && <SetupMyLoan onNext = {onNext} onPrev = {() => setState(4)} /> }
 
 
 
